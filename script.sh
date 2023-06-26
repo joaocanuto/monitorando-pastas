5da92a80a4c3499f19aaa1a343c3896cddf24d1e
João Guilherme  Araujo Canuto <joaogacanuto@gmail.com>
2023-06-26 01:50:24 -0300

    Updating comments

--- script.sh
+++ script.sh
@@ -19,26 +19,41 @@ cp -r "$source_folder" "$backup_folder"
 
 #Função Criada para lidar com eventos
 handle_event() {
-	local event_path="$1"
-	local event_filename=$(basename "$event_path")
-	local event_type="$2"
-
 	# Sincronizando minha pasta original com minha pasta de backups.
 	rsync -a --delete "$source_folder/" "$backup_folder/"
 	
 	#Verificando se a rsync encontrou algum erro.
 	rsync_exit_code=$?
 	if [ $rsync_exit_code -ne 0 ]; then
-	    echo "Error: rsync failed with exit code $rsync_exit_code"
+	  echo "Error: rsync failed with exit code $rsync_exit_code"
 	fi
 }
 
 
 
 # Monitoramento da minha source_folder usando inotify
+# -m monitoramento continuo
+# -r monitorament recursivo
+# -e eventos a serem monitorados
+# Eventos : 
+# create -> evento acionado quando um arquivo ou pasta é criado
+# delete -> evento acionado quando um arquivo ou pasta é deletado
+# modify -> evento acionado quando um arquivo ou pasta  é modificado. 
+# : Qualquer alteração feita em um arquivo existente, como alterar seu conteúdo ou metadados, acionará o evento modify.
+# move -> evento acionado quando um arquivo ou pasta é movido.
+# : Se um arquivo for movido de um local para outro ou renomeado, o evento move será detectado
+# close_write -> evento acionado quando um arquivo é fechado após ter sido aberto para gravação.
+# : Ele ocorre quando um processo conclui a escrita em um arquivo na pasta monitorada e fecha o arquivo.
 inotifywait -m -r -e create,delete,modify,move,close_write  "$source_folder"  |
-while read -r directory event filename; do
-    handle_event "$event_path" "$event"
+while read -r directory event filename; do 
+    #itera sobre cada linha de saída do comnando inotifywait 
+    # read -r -> É um comando do shell que lê uma linha de entrada e atribui os valores às variáveis especificadas.
+    # directory -> É uma variável que receberá o valor do diretório relacionado ao evento.
+    # event -> É uma variável que receberá o tipo de evento ocorrido.
+    # filename -> É uma variável que receberá o nome do arquivo relacionado ao evento.
+
+    handle_event 
+
     #Salvando as alterações no Log
     echo "$(date) - $event - $filename" >> "$log_file"
 done
